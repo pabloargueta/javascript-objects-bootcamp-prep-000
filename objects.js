79854e503729167c263d292db2d64511139dbccf
Pablo Argueta <pabloargueta27@gmail.com>
2018-05-15 11:44:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,17 @@
 var playlist = {
+<<<<<<< HEAD
   'artist Names' : 'song names'
 };
 
 
 function updatePlaylist(playlist, artistName, songTitle){
+=======
+  
+};
+
+
+function playlist(playlist, artistName, songTitle){
+>>>>>>> 874eb0bcf09cd034b4870ab096502dca9872c172
   playlist[artistName] = songTitle;
   return playlist;
 }
